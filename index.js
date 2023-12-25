e1c2b2ac6514a20bef319766f79b7fe748031038
Blake Skor <blake.skor@outlook.com>
2023-12-24 19:43:02 -0500

    Output SVG

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const fs = require("fs").promises;
 const path = require("path");
+const promptSVG = require("./lib/userInput");
 // THEN I can enter up to three characters
-// WHEN I am prompted for the text color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I am prompted for a shape
-// THEN I am presented with a list of shapes to choose from: circle, triangle, and square
-// WHEN I am prompted for the shape's color
-// THEN I can enter a color keyword (OR a hexadecimal number)
-// WHEN I have entered input for all the prompts
+(async () => {
+	const svg = await promptSVG();
+	await fs.writeFile(path.join("Output", "logo.svg"), svg);
+	console.log("Generated logo.svg");
+})();
+
 // THEN an SVG file is created named `logo.svg`
 // AND the output text "Generated logo.svg" is printed in the command line
 // WHEN I open the `logo.svg` file in a browser
